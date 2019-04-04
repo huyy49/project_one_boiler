95b4f9c9cf034b0f634bbd50b031bc4ea13e6493
Fabian Flores <ffflores1@outlook.com>
2019-04-04 16:40:56 -0400

    conformed modifyTitleTag() function to linter suggestions. changed function callbacks to es6 arrow syntax per linter suggestion

--- utils/generateScreenAndRoute.js
+++ utils/generateScreenAndRoute.js
@@ -5,6 +5,21 @@ const baseFile = path.join('utils/base.html')
 const copiedFile = path.join(`public/html/${process.argv[2]}.html`)
 const newRoute = `app.get('/${process.argv[2]}', (req, res) => res.sendFile(html_dir + \'${process.argv[2]}.html\', { root: __dirname }))\n`
 
+const modifyTitleTag = () => {
+  const titleParam = process.argv[2];
+  const newTitle = `${titleParam.charAt(0).toUpperCase()}${titleParam.slice(1)}`;
+
+  fs.readFile(copiedFile, 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    const replacedTitle = data.replace(/<title>(.*?)<\/title>/g, `<title>${newTitle}</title>`);
+    fs.writeFile(copiedFile, replacedTitle, (writeErr) => {
+      if (writeErr) throw writeErr;
+      console.log(`Title of Screen is ${newTitle}`);
+    });
+  });
+};
+
 fs.copyFile(baseFile, copiedFile, (err) => {
   if (err) throw err;
   console.log('Screen Generated!');
@@ -12,22 +27,8 @@ fs.copyFile(baseFile, copiedFile, (err) => {
   modifyTitleTag();
 });
 
-const modifyTitleTag = () => {
-  const titleParam = process.argv[2];
-  const newTitle = `${titleParam.charAt(0).toUpperCase()}${titleParam.slice(1)}`;
 
-  fs.readFile(copiedFile, 'utf-8', function (err,data) {
-    if (err) throw err;
-
-    let replacedTitle = data.replace(/<title>(.*?)<\/title>/g, `<title>${newTitle}</title>`);
-    fs.writeFile(copiedFile, replacedTitle, function (err) {
-       if (err) throw err;
-       console.log(`Title of Screen is ${newTitle}`);
-    });
-  });
-}
-
-fs.appendFile(path.join('index.js'), newRoute, function (err) {
+fs.appendFile(path.join('index.js'), newRoute, (err) => {
   if (err) throw err;
   console.log('Route Added!');
 });
